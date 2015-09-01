20facd4d9ce9f9841c3fcc42a3095ef5b81224b6
Den1eD1 <gabo.dimitrov@gmail.com>
2015-09-01 17:19:33 +0300

    Changed password security check

--- WebChat/WebChat.Services/Models/MessageBindingModels.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebChat.Services.Models
-{
-    public class MessageBindingModel
-    {
-        public string Text { get; set; }
-
-        public string PosterId { get; set; }
-    }
-}
